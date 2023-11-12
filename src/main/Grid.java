b48e63cc1a6b627c840e92c392677c287ec9658b
MusicViking <sujithroshan72001@gmail.com>
2023-11-12 18:40:58 +0530

    added class grid

---
+++ src/main/Grid.java
@@ -0,0 +1,29 @@
+package main;
+
+public class Grid {
+
+    private int x;
+    private int y;
+
+    public Grid(int x, int y) {
+        this.setX(x);
+        this.setY(y);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+}
